a2a1a1e46c88dbfeab91e7235b5f3407eb5e1615
Kalani Craig <kalani@kalanicraig.com>
2024-04-15 22:16:30 -0400

    Filter buttons on item list page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,47 @@
 $(document).foundation()
 
-// shows and hides filtered items
-$(".filter-simple-button").click(function() {
-  var value = $(this).attr('data-filter');
-  if(value === "all") {
-    $('.filter-simple-item').show('1000');
-  } else {
-    $(".filter-simple-item").not('.'+value).hide('3000');
-    $('.filter-simple-item').filter('.'+value).show('3000');
-  }
+// ItemCard button and checkbox filtering
+const filterButtons = document.querySelectorAll('.filter-button');
+const filterCheckboxes = document.querySelectorAll('.checkbox-wrapper input[type="checkbox"]'); // Update the selector here
+const itemCards = document.querySelectorAll('.filter-item');
+
+function filterItems() {
+	const selectedCategories = Array.from(filterButtons)
+		.filter(button => button.classList.contains('active'))
+		.map(button => button.dataset.category);
+
+	const selectedFormats = Array.from(filterCheckboxes)
+		.filter(checkbox => checkbox.checked)
+		.map(checkbox => checkbox.dataset.format);
+
+	itemCards.forEach(card => {
+		const itemCategories = card.dataset.categories.split(',');
+		const itemFormats = card.dataset.formats.split(',');
+
+		const showAll = selectedCategories.length === 0 && selectedFormats.length === 0;
+		const showCat = selectedCategories.some(category => itemCategories.includes(category));
+		const showForm = selectedFormats.some(format => itemFormats.includes(format));
+
+		const isPerson = selectedFormats.some(format => itemFormats.includes('person'));
+
+		// WANT TO CHANGE HOW THE FILTERS INTERACT? (I.E. AND vs OR) CHANGE THE LINE BELOW
+		if ((showCat || showForm || showAll)) {
+			card.style.display = "block";
+		} else {
+			card.style.display = "none";
+		}
+	});
+}
+
+filterButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		button.classList.toggle('active');
+		filterItems();
+	});
 });
 
-// changes active class on filter buttons
-$('.filter-simple-button').click(function () {
-  $(this).siblings().removeClass('is-active');
-  $(this).addClass('is-active');
+filterCheckboxes.forEach(checkbox => {
+	checkbox.addEventListener('change', () => {
+		filterItems();
+	});
 });
-
